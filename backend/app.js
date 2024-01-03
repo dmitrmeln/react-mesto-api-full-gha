93f0c8590843e20e937f4e5f33d0a89f06c6024c
Dmitrii Melnichuk <melnichuk176@gmail.com>
2024-01-03 17:05:11 +0300

    project 15: rate-limit added

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 const cors = require('cors');
 
 const app = express();
@@ -13,11 +14,21 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT, MONGO_URL } = require('./utils/config');
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  // store: ... , // Use an external store for consistency across multiple server instances.
+});
+
 mongoose.connect(MONGO_URL)
   .then(() => {
     console.log('mongodb connected');
   });
 
+// Apply the rate limiting middleware to all requests.
+app.use(limiter);
 app.use(cors());
 app.use(express.json());
 app.use(helmet());
